ee1c27c9d97bd310f14146608a5509cd4b1348f4
Anthony Ngo <anthonyngo002@gmail.com>
2024-05-27 21:29:33 -0700

    fix buildTestSuite

--- buildTestSuite.ps1
+++ buildTestSuite.ps1
@@ -3,9 +3,14 @@ $Env:auth0_domain = "dev-ahjkm0k5e1e7ik74.us.auth0.com"
 $Env:client_id = "SHtECfwsr1cV0DznVVyoValV7QBkxAbf"
 $Env:client_secret = "hClEGpfL_qxQz2ZAP6Ss2hZHMifHMkwkDjYv30pcFEjlqdq2ruuQXBNDCQABnqBp"
 
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+try {
+  scoop update
+} catch {
+  Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+  Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+}
 
+# fix error where scoop needs git
 scoop install git
 
 scoop install postgresql
@@ -22,5 +27,11 @@ scoop install nodejs
 npm install
 npm install --dev
 
-Start-Process "" pipenv run python app.py
-pipenv run pytest --cov=app --cov=db --cov-report=term
+# start app
+$app = Start-Job { pipenv run python app.py }
+
+pipenv run pytest .\tests\test_app.py --cov=app --cov=db --cov-report=term
+
+# stop app
+Stop-Job $app
+Remove-Job $app
