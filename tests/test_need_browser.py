ff09303691345655d732385bac54510d9dbd2ebe
Quang Tran <helsytran@gmail.com>
2024-05-27 19:10:31 -0700

    QT: Add test_add_solved_comment()

--- tests/test_need_browser.py
+++ tests/test_need_browser.py
@@ -30,7 +30,7 @@ class TestPost:
       "title": "automated drawing",
       "description": "",
       "hint": "",
-      "word-selection": "shotgun",
+      "word-selection": self.post_solution,
       "drawing_tags": "firearm"
     }
     response = self.client.post("/drawing", data=data)
@@ -56,6 +56,54 @@ class TestPost:
       }
     response = self.client.get(f"/post/{self.post_id}/edit")
     assert response.status_code == 403
+
+  def test_add_solved_comment(self):
+    post = db.get_post(self.post_id)
+    with self.client.session_transaction() as session:
+      session["profile"] = {
+        "user_id": "auth0|662b504a5dea8e9dfd414e67",
+        "name": "test@example.com"
+      }
+    data = {
+      "answer": self.post_solution
+    }
+    response = self.client.post(f"/post/{self.post_id}", data=data)
+
+    post = db.get_post(self.post_id)
+    
+    assert post["solved"] == True
+    assert response.status_code == 302
+    
+    
+  # def test_delete_comment(self):
+  #   post = db.get_post(self.post_id)
+  #   with self.client.session_transaction() as session:
+  #     session["profile"] = {
+  #       "user_id": "auth0|662b504a5dea8e9dfd414e67",
+  #       "name": "test@example.com"
+  #     }  
+  #   # Add a comment to be deleted
+  #   comment_content = "shotgun"
+  #   comments = db.get_comments(self.post_id)
+  #   comment_id = next(comment['id'] for comment in comments if comment['content'] == comment_content)
+        
+     
+  #   with self.client.session_transaction() as session:
+  #       session["profile"] = {
+  #           "user_id": user_id,
+  #           "name": "test@example.com"
+  #        }
+        
+  #   data = {
+  #       "comment_id": comment_id
+  #   }
+  #   response = self.client.post('/post/deleteComment/', data=data)
+
+  #   # Check that the response indicates success
+  #   assert response.data.decode() == "Success"
+        
+  #   comments_after_deletion = db.get_comments(self.post_id)
+  #   assert not any(comment['id'] == comment_id for comment in comments_after_deletion)
 
   def test_edit_post(self):
     post = db.get_post(self.post_id)
